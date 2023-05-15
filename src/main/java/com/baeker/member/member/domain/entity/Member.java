d9ba6b2eb32e9cb37a22ddb2d90db9a4dba88958
choideakook <shdrnrhd113@naver.com>
2023-05-15 14:35:31 +0900

    feat: member domain business logic 구현

--- src/main/java/com/baeker/member/member/domain/entity/Member.java
+++ src/main/java/com/baeker/member/member/domain/entity/Member.java
@@ -1,17 +1,16 @@
 package com.baeker.member.member.domain.entity;
 
-import io.micrometer.core.instrument.util.AbstractPartition;
+import com.baeker.member.member.domain.entity.dto.BaekJoonDto;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -36,4 +35,58 @@ public class Member extends BaseEntity {
     @Builder.Default
     @OneToMany(mappedBy = "member")
     private List<MemberSnapshot> snapshotList = new ArrayList<>();
+
+    //-- crate method --//
+    protected static Member createMember(String provider, String username, String name, String about, String password, String profileImg, String email, String token) {
+        return builder()
+                .provider(provider)
+                .username(username)
+                .nickname(name)
+                .about(about)
+                .password(password)
+                .profileImg(profileImg)
+                .kakaoProfileImage(profileImg)
+                .email(email)
+                .accessToken(token)
+                .newMember(true)
+                .build();
+    }
+
+
+    //-- business logic --//
+
+    // name, about, profileImg 수정 //
+    protected Member modifyMember(String name, String about, String img) {
+        return this.toBuilder()
+                .nickname(name)
+                .about(about)
+                .profileImg(img)
+                .modifyDate(LocalDateTime.now())
+                .newMember(false)
+                .build();
+    }
+
+    // 첫방문 회원 체크 //
+    protected void joinComplete() {
+        this.newMember = false;
+    }
+
+    // 백준 아이디 등록 //
+    protected Member connectBaekJoon(String baekJoonName) {
+        return this.toBuilder()
+                .baekJoonName(baekJoonName)
+                .build();
+    }
+
+    // 백준 점수 최신화 //
+    protected Member updateBaeJoon(BaekJoonDto dto) {
+        return this.toBuilder()
+                .bronze(this.getBronze() + dto.getBronze())
+                .sliver(this.getSliver() + dto.getSliver())
+                .gold(this.getGold() + dto.getGold())
+                .diamond(this.getDiamond() + dto.getDiamond())
+                .ruby(this.getRuby() + dto.getRuby())
+                .platinum(this.getPlatinum() + dto.getPlatinum())
+                .build();
+    }
 }
---
+++ src/main/java/com/baeker/member/member/domain/entity/dto/BaekJoonDto.java
@@ -0,0 +1,16 @@
+package com.baeker.member.member.domain.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BaekJoonDto {
+
+    int bronze;
+    int sliver;
+    int gold;
+    int diamond;
+    int ruby;
+    int platinum;
+}
